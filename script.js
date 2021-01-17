e86e7d9a350c09fb11471353a3a1377223315fd4
Eliana Moreno <eliana.morenoestrada@gmail.com>
2021-01-16 21:15:19 -0500

    Dark Mode

---
+++ script.js
@@ -0,0 +1,24 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById ('image1');
+const image2 =  document.getElementById ('image2');
+const image3 =  document.getElementById ('image3');
+const textBox = document.getElementById ('text-box');
+
+// Dark Mode 
+
+
+// Swicth Theme Dynamically 
+function switchTheme (event){
+   if (event.target.checked){
+       document.documentElement.setAttribute('data-theme', 'dark');
+       darkMode();
+   } else {
+       document.documentElement.setAttribute('data-theme', 'light');
+       lightMode();
+   }
+}
+
+//Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
